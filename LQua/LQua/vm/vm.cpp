aa904fdcb0e968c910843644f2e400b0363177bc
unknown <hudaqing@cyou-inc.com>
2019-11-01 18:27:36 +0800

    s

--- LQua/LQua/vm/vm.cpp
+++ LQua/LQua/vm/vm.cpp
@@ -4,3 +4,56 @@ int VM::Run(LQState* ls, ObjectProto * proto)
 {
 	return 0;
 }
+
+int VM::Execute(LQState* ls, ObjectProto* proto, int base)
+{
+
+}
+
+int VM::CallCfunc(LQState* ls, CFunction func, int base)
+{
+
+}
+
+Number VM::Caculate(Number n1, Number n2, OpCode op_code)
+{
+	switch (op_code)
+	{
+	case OP_ADD:
+		return n1 + n2;
+	case OP_SUB:
+		return n1 - n2;
+	case OP_MUL:
+		return n1 * n2;
+	case OP_DIV:
+		return n1 / n2;
+	default:
+		break;
+	}
+	return 0;
+}
+
+bool VM::Compare(Number n1, Number n2, OpCode op_code)
+{
+
+}
+
+void VM::Concat(LQState* ls, const TObject& o1, const TObject& o2, TObject& result)
+{
+
+}
+
+void VM::Call(LQState* ls, int param_nums, int result_nums)
+{
+
+}
+
+void VM::AdjustTop(LQState* ls, int new_top)
+{
+
+}
+
+int VM::GetLine(ObjectProto* proto, int pc)
+{
+
+}
--- LQua/LQua/vm/vm.h
+++ LQua/LQua/vm/vm.h
@@ -1,6 +1,8 @@
 #pragma once
 
 #include "../base/ldata.h"
+#include "../base/lobject.h"
+#include "../base/basedef.h"
 
 class LQState;
 
@@ -9,4 +11,12 @@ class VM
 public:
 	static int Run(LQState* ls, ObjectProto* proto);
 private:
+	static int Execute(LQState* ls, ObjectProto* proto, int base);
+	static int CallCfunc(LQState* ls, CFunction func, int base);
+	static Number Caculate(Number n1, Number n2, OpCode op_code);
+	static bool Compare(Number n1, Number n2, OpCode op_code);
+	static void Concat(LQState* ls, const TObject& o1, const TObject& o2, TObject& result);
+	static void Call(LQState* ls, int param_nums, int result_nums);
+	static void AdjustTop(LQState* ls, int new_top);
+	static int GetLine(ObjectProto* proto, int pc);
 };
